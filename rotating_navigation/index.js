d621c623c7533dd517bb90f4c685d3607ea177c4
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-12-08 19:58:54 +0530

    Added Javascript to rotating navigation

--- rotating_navigation/index.js
+++ rotating_navigation/index.js
@@ -1,3 +1,20 @@
 {
-    let btn = document.querySelector('.nav_button');
+    let open_btn = document.querySelector('.nav_button.open');
+    let close_btn = document.querySelector('.nav_button.close');
+
+    let art = document.querySelector('.article');
+    
+    open_btn.addEventListener("click", (event) => {
+        art.classList.remove('rotate-back');
+        art.classList.add('rotate-for');
+        close_btn.classList.remove('hidden');
+        open_btn.classList.add('hidden');
+    });
+
+    close_btn.addEventListener("click", (event) => {
+        art.classList.remove('rotated-for');
+        art.classList.add('rotate-back');
+        open_btn.classList.remove('hidden');
+        close_btn.classList.add('hidden');
+    });
 }
