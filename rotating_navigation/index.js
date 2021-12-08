692b948d2f9a7422ead6cf19bf05911b51c1938e
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-12-08 20:27:31 +0530

    perfected animation for rotating navigation

--- rotating_navigation/index.js
+++ rotating_navigation/index.js
@@ -1,6 +1,7 @@
 {
     let open_btn = document.querySelector('.nav_button.open');
     let close_btn = document.querySelector('.nav_button.close');
+    let nav = document.querySelector('.main');
 
     let art = document.querySelector('.article');
     
@@ -9,6 +10,9 @@
         art.classList.add('rotate-for');
         close_btn.classList.remove('hidden');
         open_btn.classList.add('hidden');
+        nav.classList.remove('out');
+        nav.classList.add('in');
+        
     });
 
     close_btn.addEventListener("click", (event) => {
@@ -16,5 +20,7 @@
         art.classList.add('rotate-back');
         open_btn.classList.remove('hidden');
         close_btn.classList.add('hidden');
+        nav.classList.remove('in');
+        nav.classList.add('out');
     });
 }
