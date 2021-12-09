77e7e2da469dff3a1223db6559f7523b91793232
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-12-09 22:38:49 +0530

    Minor changes to scroll animation

--- scroll_animation/index.js
+++ scroll_animation/index.js
@@ -1,30 +1,20 @@
 {
     const boxes = document.querySelectorAll('.box');
     const trig = window.innerHeight * 0.8;
-    
-    window.onload = () => {
-        boxes.forEach((box) => {
-            const btop = box.getBoundingClientRect().top;
-    
-            if(btop < trig){
-                box.classList.add('show');
-            }else{
-                box.classList.remove('show');
-            }
-        });
-    };
-    
-    window.addEventListener("scroll", () => {
-        console.log(window.innerHeight);
-        
-        boxes.forEach((box) => {
-            const btop = box.getBoundingClientRect().top;
 
+    check();
+    
+    function check(){
+        boxes.forEach((box) => {
+            const btop = box.getBoundingClientRect().top;
+    
             if(btop < trig){
                 box.classList.add('show');
             }else{
                 box.classList.remove('show');
             }
         });
-    });
+    }
+    
+    window.addEventListener("scroll", check);
 }
