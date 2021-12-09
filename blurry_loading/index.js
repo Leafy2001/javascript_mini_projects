750b4565bde03e51411c5b9ce590b26861da71ae
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-12-09 15:07:25 +0530

    blurry loading done

--- blurry_loading/index.js
+++ blurry_loading/index.js
@@ -1,3 +1,26 @@
 {
+    const loadText = document.querySelector('.ld-txt');
+    const bg = document.querySelector('.bg');
+
+    let load = 0;
+    
+    let int = setInterval(blurring, 30);
+
+    function blurring(){
+        load++;
+
+        if(load === 100){
+            clearInterval(int);
+            loadText.innerHTML = "";
+            bg.style.top = "0px";
+            bg.style.left = "0px";
+            bg.style.width = "100vw";
+            bg.style.height = "100vh";
+            return;
+        }
+
+        bg.style.filter = "blur(" + (100-load) + "px)";
+        loadText.innerHTML = load + "%";
+    }
     
 }
